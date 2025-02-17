61302b0ebcdc1481de68cd4b3bec9da367e9a887
m1erla <116915007+m1erla@users.noreply.github.com>
2025-02-17 23:13:49 +0300

    Added new icons and images, revised app.json attributes.

--- constants/icons.ts
+++ constants/icons.ts
@@ -1,5 +1,11 @@
 import google from "@/assets/icons/google.png";
+import search from "@/assets/icons/search.png";
+import home from "@/assets/icons/home.png";
+import person from "@/assets/icons/person.png";
 
 export default {
-    google,
-}
+  google,
+  search,
+  home,
+  person
+};
